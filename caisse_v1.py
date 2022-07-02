5bd4af9c9dc22492acea1c34f7d0769dd916c3e3
codecaiine <akabrouyannickn@gmail.com>
2022-07-02 22:01:46 +0000

    Changer reste en monnaie

--- caisse_v1.py
+++ caisse_v1.py
@@ -22,36 +22,36 @@ while prix_article !=0:
 
 somme_remis = int(input('Saisissez le somme rémis par le client : '))
 print("Le client a rémis")
-reste = somme_remis - total_achats
-print("Il vous reste ", reste, " FCFA comme monnaie restant .")
+monnaie = somme_remis - total_achats
+print("Il vous monnaie ", monnaie, " FCFA comme monnaie restant .")
 
-while reste > 10000:
+while monnaie > 10000:
     dix_mille +=1
-    reste-= 10000
-while reste > 5000:
+    monnaie-= 10000
+while monnaie > 5000:
     cinq_mille +=1
-    reste-= 5000
-while reste > 2000:
+    monnaie-= 5000
+while monnaie > 2000:
     deux_mille +=1
-    reste-= 2000
-while reste > 1000:
+    monnaie-= 2000
+while monnaie > 1000:
     mille +=1
-    reste-= 1000
-while reste > 500:
+    monnaie-= 1000
+while monnaie > 500:
     cinq_cent +=1
-    reste-= 500
-while reste > 200:
+    monnaie-= 500
+while monnaie > 200:
     deux_cent +=1
-    reste-= 200
-while reste > 100:
+    monnaie-= 200
+while monnaie > 100:
     cent +=1
-    reste-= 100
-while reste > 50:
+    monnaie-= 100
+while monnaie > 50:
     cinqante +=1
-    reste-= 50
-while reste > 25:
+    monnaie-= 50
+while monnaie > 25:
     vingt_cinq +=1
-    reste-= 25
+    monnaie-= 25
 
 print('Il y a {} billet(s) de 10.000 FCFA'.format(dix_mille))
 print('Il y a {} billet(s) de 5.000 FCFA'.format(cinq_mille))
@@ -63,16 +63,16 @@ print('Il y a {} pièce(s) de 100 FCFA'.format(cent))
 print('Il y a {} pièce(s) de 50 FCFA'.format(cinqante))
 print('Il y a {} pièce(s) de 25 FCFA'.format(vingt_cinq))
 
-if reste == 10:
-    print("Il vous reste ", reste," FCFA" ,"Malheureusement nous n'avons pas de monnaie .")
+if monnaie == 10:
+    print("Il vous monnaie ", monnaie," FCFA" ,"Malheureusement nous n'avons pas de monnaie .")
     print("Nous pouvons vous offrir des BONBONS à la place .")
-elif reste == 5:
-    print("Il vous reste ", reste," FCFA" ,"Malheureusement nous n'avons pas de monnaie .")
+elif monnaie == 5:
+    print("Il vous monnaie ", monnaie," FCFA" ,"Malheureusement nous n'avons pas de monnaie .")
     print("Nous pouvons vous offrir des BONBONS à la place .")
 else:
-    print("La monnaie est : ", reste, "FCFA")
+    print("La monnaie est : ", monnaie, "FCFA")
 
-if reste%5 == 0:
+if monnaie%5 == 0:
     print("Merci pour votre achat ...")
 else:
     print("Pas de monnaie disponible ...")
