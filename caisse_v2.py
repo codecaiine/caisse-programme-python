715e2668cd55daede310bd0ed5dc817fe59c4a16
codecaiine <akabrouyannickn@gmail.com>
2022-07-03 21:09:27 +0000

    Calculer la monnaie de 10000

--- caisse_v2.py
+++ caisse_v2.py
@@ -26,6 +26,10 @@ for client in range(1,3):
         print("Le client a rémis")
         monnaie = montant_client - total_achats
         print("Il vous reste ", monnaie, " FCFA comme monnaie restant .")
+        
+        while monnaie > 10000:
+            dix_mille +=1
+            monnaie-= 10000
 
         total_recettes += total_achats
 print("Total recette", total_recettes)
