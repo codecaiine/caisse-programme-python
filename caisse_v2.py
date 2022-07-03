30743cddcc110c3056722d76bbed9f9107e87f67
codecaiine <akabrouyannickn@gmail.com>
2022-07-03 21:18:15 +0000

    Afficher la monnaie de 500

--- caisse_v2.py
+++ caisse_v2.py
@@ -59,6 +59,7 @@ for client in range(1,3):
         print('Il y a {} billet(s) de 5.000 FCFA'.format(cinq_mille))
         print('Il y a {} billet(s) de 2.000 FCFA'.format(deux_mille))
         print('Il y a {} billet(s) de 1.000 FCFA'.format(mille))
+        print('Il y a {} billets de 500 FCFA'.format(cinq_cent))
 
 
         total_recettes += total_achats
