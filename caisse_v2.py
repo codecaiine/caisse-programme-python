efd4588bce4ce15de0ce7be035f8c1b65be3588d
codecaiine <akabrouyannickn@gmail.com>
2022-07-03 21:15:27 +0000

    Calculer la monnaie de 25

--- caisse_v2.py
+++ caisse_v2.py
@@ -51,6 +51,9 @@ for client in range(1,3):
         while monnaie > 50:
             cinqante +=1
             monnaie-= 50
+        while monnaie > 25:
+            vingt_cinq +=1
+            monnaie-= 25
 
         total_recettes += total_achats
 print("Total recette", total_recettes)
